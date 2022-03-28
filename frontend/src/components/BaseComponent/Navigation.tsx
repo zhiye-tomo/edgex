d6cde2551f839227637d36a3c674fa0f784ad4f4
西田智也 <tomoya.nishida@shuuyasukinoMacBook-Pro.local>
2022-03-28 13:02:48 +0900

    DONE: Made footer flexbox

--- frontend/src/components/BaseComponent/Navigation.tsx
+++ frontend/src/components/BaseComponent/Navigation.tsx
@@ -1,14 +1,28 @@
 import styles from "../../styles/components/Navigation.module.scss";
 
+type Nav = {
+  name: string;
+  link: string;
+};
+
 export const Navigation: React.FC = () => {
+  const navs: Array<Nav> = [
+    { name: "Top", link: "xxx" },
+    { name: "Search", link: "xxx" },
+    { name: "New", link: "xxx" },
+    { name: "XXX", link: "xxx" },
+    { name: "Me", link: "xxx" },
+  ];
   return (
     <nav>
       <ul className={styles.lists}>
-        <li className={styles.li}>list1</li>
-        <li className={styles.li}>list2</li>
-        <li className={styles.li}>list3</li>
-        <li className={styles.li}>list4</li>
-        <li className={styles.li}>list5</li>
+        {navs.map((nav: Nav, key: number) => {
+          return (
+            <li className={styles.li} key={key}>
+              <a className={styles.link}>{nav.name}</a>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
